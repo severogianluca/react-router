05a1573c9285ed67410d06d6a9c38a799d345de9
severogianluca <gianlucasevero300@gmail.com>
2025-04-16 20:30:00 +0200

    Feat_Create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layout/DefaultLayOut";
+import HomePage from "./pages/HomePage";
+import ChiSiamo from "./pages/ChiSiamo";
+import ListaDeiPost from "./pages/ListaDeiPost";
 
 function App() {
-
-
   return (
-    <>
+    <BrowserRouter>
+      <Routes>
 
+        <Route path="/" element={<DefaultLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="ChiSiamo" element={<ChiSiamo />} />
+          <Route path="ListaDeiPost" element={<ListaDeiPost />} />
+        </Route>
 
-
-    </>
-  )
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
